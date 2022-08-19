719eae52ea2771588b69efadff25943f75a7bcd8
Dmitry Kovalenko <dmitry.kovalenko.mu@gmail.com>
2022-08-19 04:14:15 +0300

    edit readme, add express, upadte package json and gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,6 @@ module.exports = {
         'browser': true,
         'es2021': true
     },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
     'ignorePatterns': ['node_modules/'],
     'rules': {
         'indent': [
